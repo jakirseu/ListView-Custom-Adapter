61802ef775983635570a9b0deb1097c84b4f2287
Jakir Hossain <jakirseu@gmail.com>
2019-11-30 12:10:21 +0400

    update project

--- app/src/main/java/me/jakir/listviewcustomadaptar/MainActivity.java
+++ app/src/main/java/me/jakir/listviewcustomadaptar/MainActivity.java
@@ -1,8 +1,9 @@
 package me.jakir.listviewcustomadaptar;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
